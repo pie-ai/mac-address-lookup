5bc05972886fad0e4460e567c12308a39b7a36b0
Patrick Stricker <github.com@pa2.de>
2016-11-13 19:25:19 +0000

    fixed to create cache dir

--- vendor-lookup.sh
+++ vendor-lookup.sh
@@ -2,7 +2,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 MAC=${1}
-CACHE_ENTRY="${DIR}/cache/${MAC}.vendor"
+CACHE_DIR="${DIR}/cache"
+CACHE_ENTRY="${CACHE_DIR}/${MAC}.vendor"
+
+if [ ! -d ${CACHE_DIR} ]; then
+	mkdir -p ${CACHE_DIR}
+fi
 
 if [ -f ${CACHE_ENTRY} ]; then
 	VENDOR=$(cat ${CACHE_ENTRY})
